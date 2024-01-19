1cd69ebcf9da720a1f3dffce9735e5b2b740c2f0
jorgelpat <jorgelpatino724@gmail.com>
2024-01-19 12:13:00 -0500

    1 Saludo

---
+++ 1.saludo.py
@@ -0,0 +1,4 @@
+#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+name = str(input("Indique su nombre"))
+saludo = f"Hola, {name}"
+print(saludo)
